66621e55f7b731f2a12ae673f9db6527c6e6e7e2
henryle97 <lehoang2797@gmail.com>
2024-03-11 15:31:59 +0700

    feat: zsh + p10k theme

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -e
+
+cp .zshrc ~/
+cp .p10k.zsh ~/
+
+wget https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim -O ~/.vim/colors/monokai.vim
+
+if ! [ -d ~/.oh-my-zsh/custom/themes ]; then
+  mkdir ~/.oh-my-zsh/custom/themes
+fi
+
+if ! [ -d ~/.oh-my-zsh/custom/themes ]; then
+  mkdir -p ~/.oh-my-zsh/custom/themes
+fi
+
+if ! [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+  git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+else
+  cd ~/.oh-my-zsh/custom/themes/powerlevel10k
+  git pull
+fi
+
+if ! [ -d ~/.oh-my-zsh/custom/plugins ]; then
+  mkdir ~/.oh-my-zsh/custom/plugins
+fi
+
+if ! [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+else
+  cd ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git pull
+fi
+
+if ! [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+else
+  cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  git pull
+fi
+
+if ! [ -d ~/.oh-my-zsh/custom/plugins/conda-zsh-completion ]; then
+  git clone https://github.com/conda-incubator/conda-zsh-completion.git ~/.oh-my-zsh/custom/plugins/conda-zsh-completion
+else
+  cd ~/.oh-my-zsh/custom/plugins/conda-zsh-completion
+  git pull
+fi
+
+# if command -v conda &> /dev/null
+# then
+#     conda init --all
+# fi
