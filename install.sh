aae477be810153ba4880a3535f5ff7e1f510685a
henryle97 <lehoang2797@gmail.com>
2024-03-11 17:30:58 +0700

    fix: update config

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ cd fonts
 cd .. && rm -rf fonts
 
 # oh-my-zsh & plugins
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
